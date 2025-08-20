7b629a6c7e5c74dcb336ab260bcc168ae73b4245
woe's child <nevermoorn@gmail.com>
2025-08-20 06:39:31 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline text-[orange]">
+      testing tailwind css...
+    </h1>
+  )
+}
