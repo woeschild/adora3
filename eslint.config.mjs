20ce61bd11bbbc3b9b0f5cedbbcce3ad3567457e
woe's child <nevermoorn@gmail.com>
2025-09-20 11:14:04 +0100

    UI version 1 complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,11 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
-  },
+  extends: "next/core-web-vitals",
+  rules: {
+    "react/no-unescaped-entities": "off"
+  }
+}
 ];
 
 export default eslintConfig;
